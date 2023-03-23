f2a361de450458e88a31a5b1a51d403235f5422a
100MilWill <100MilWill@users.noreply.github.com>
2023-03-23 19:37:07 +0000

    deploy: c1b81909366d0dc4568839a3d7a581bc6245606a

---
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -0,0 +1,20 @@
+
+ //Custom Text Header //
+ // ACSII slant font
+
+// Needs to be inside <section id="login" class="shadow">
+// YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
+
+let custom_text_header = `
+<div class="custom-text-header"> <pre>                                                          
+  888  888    d888   .d8888b.   .d8888b.       888b     d888 d8b 888           888       888 d8b 888      888      
+  888  888   d8888  d88P  Y88b d88P  Y88b      8888b   d8888 Y8P 888           888   o   888 Y8P 888      888      
+888888888888   888  888    888 888    888      88888b.d88888     888           888  d8b  888     888      888      
+  888  888     888  888    888 888    888      888Y88888P888 888 888           888 d888b 888 888 888      888      
+  888  888     888  888    888 888    888      888 Y888P 888 888 888           888d88888b888 888 888      888      
+888888888888   888  888    888 888    888      888  Y8P  888 888 888           88888P Y88888 888 888      888      
+  888  888     888  Y88b  d88P Y88b  d88P      888   "   888 888 888           8888P   Y8888 888 888      888      
+  888  888   8888888 "Y8888P"   "Y8888P"       888       888 888 88888888      888P     Y888 888 88888888 88888888  
+                                                                 </pre> </div>
+`;
+document.getElementById("login").innerHTML += custom_text_header
